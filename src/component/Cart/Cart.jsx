4af60fa8974f7cbd137a13b0c4abb3dceca6a794
Abu Hosain <cryptichasan925@gmail.com>
2023-05-09 13:09:19 +0600

    show data from cart

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -6,9 +6,15 @@ const Cart = ({cart}) => {
     // const {cart} = props // option 2
     let total = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for(const product of cart){
-        total = total + product.price;
-        totalShipping = totalShipping + product.shipping
+        // if(product.quantity === 0){
+        //     product.quantity ==1;
+        // }
+        // product.quantity = product.quantity || 1;
+        total = total + product.price * product.quantity;
+        totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity
     }
     const tax = total * 7 /100;
     const grandTotal = total + totalShipping + tax;
@@ -16,7 +22,7 @@ const Cart = ({cart}) => {
     return (
         <div className='cart'>
             <h4>Order summery</h4>
-            <p>Selected items:{cart.length}</p>
+            <p>Selected items:{quantity}</p>
             <p>Total Price:{total}</p>
             <p>Total Shipping:{totalShipping}</p>
             <p>Tax:{tax}</p>
